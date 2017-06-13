a8738b64b829372cb77d39a7af25bd946ebac19e
Claudio Cicali <claudio.cicali@number26.de>
2017-06-13 13:11:51 +0200

    Fixes a double quotes issue

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,5 +3,5 @@ import './style.css'
 
 // React.SFC is a shorthand for React.StatelessComponent (plus the F of Functional)
 export const Header:React.SFC<React.Props<any>> = () => (
-  <h1 className='Header'>React with TypeScript: a starter kit</h1>
+  <h1 className="Header">React with TypeScript: a starter kit</h1>
 )
