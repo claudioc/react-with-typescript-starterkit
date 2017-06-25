a233127b87a1cb8e061c3325fcefbbc1d9990ffe
Claudio Cicali <claudio.cicali@gmail.com>
2017-06-25 13:17:21 +0200

    Adds basic support to css modules

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,13 +1,13 @@
 import * as React from 'react'
 import { Header } from '../Header'
 import { getTime } from '../../lib/getTime'
-import './style.css'
+import * as style from './style.css'
 
 // The second paramenter of the extended class is the state object ('undefined' in this case)
 export class App extends React.Component<React.Props<any>, undefined> {
   public render() {
     return (
-      <div className="App">
+      <div className={style.App}>
         <Header/>
         <img src="assets/images/ts-react.png"/>
         <p>It's around {getTime()} and everything is fine.</p>
